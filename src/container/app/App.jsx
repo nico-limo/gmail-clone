0b6032142dc502971b715c842cf4346f116d788b
nico-limo <nicolas.limonoff@gmail.com>
2021-04-06 22:51:53 -0300

    set proyecto con redux

---
+++ src/container/app/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//CSS
+import './App.css';
+//Components
+import Header from "../header/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header/>
+      <h2>Lets build gmail</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/container/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+//CSS
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      <h2>Header</h2>
+    </div>
+  );
+};
+
+export default Header;
