8ca01cc07fd112e6ca839dbdc403fa7fe2c5da95
nico-limo <nicolas.limonoff@gmail.com>
2021-04-08 23:59:15 -0300

    sidebar done, working in components email and emaillist

--- src/container/app/App.jsx
+++ src/container/app/App.jsx
@@ -4,14 +4,28 @@ import './App.css';
 //Components
 import Header from "../header/Header";
 import Sidebar from '../sidebar/Sidebar';
+//React Router dom
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <div className="app">
       <Header/>
+
+      <div className="app__body">
       <Sidebar/>
-      <h2>Lets build gmail</h2>
+      <Switch>
+        <Route path="/mail">
+          <Mail/>
+        </Route>
+        <Route path="/">
+          <EailList/>
+        </Route>
+      </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
--- src/container/sidebar/Sidebar.jsx
+++ src/container/sidebar/Sidebar.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 //CSS
 import "./Sidebar.css";
+//Components
+import SidebarOption from "../../components/sidebaroption/SidebarOption";
 //Material UI
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
+
 const Sidebar = () => {
   const iconAdd = () => <AddIcon fontSize="large" />;
 
@@ -12,7 +25,29 @@ const Sidebar = () => {
       <Button className="sidebar__compose" startIcon={iconAdd()}>
         Redactar
       </Button>
-      <h1>SIDEBAR</h1>
+
+    <SidebarOption Icon={InboxIcon} title="Recibidos" number={54} selected={true} />
+    <SidebarOption Icon={StarIcon} title="Destacados" number={54} />
+    <SidebarOption Icon={AccessTimeIcon} title="Pospuestos" number={54} />
+    <SidebarOption Icon={LabelImportantIcon} title="Importantes" number={54} />
+    <SidebarOption Icon={NearMeIcon} title="Enviados" number={54} />
+    <SidebarOption Icon={NoteIcon} title="Borradores" number={54} />
+    <SidebarOption Icon={ExpandMoreIcon} title="Más" number={54} />
+
+    <div className="sidebar__footer">
+      <div className="sidebar__footerIcons">
+        <IconButton>
+          <PersonIcon />
+        </IconButton>
+        <IconButton>
+          <DuoIcon />
+        </IconButton>
+        <IconButton>
+          <PhoneIcon />
+        </IconButton>
+      </div>
+    </div>
+
     </div>
   );
 };
